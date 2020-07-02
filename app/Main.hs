51edde2a52ffda5585f0c3098fb0604a2829e269
Alexey Kirpichnikov <alex.kirp@gmail.com>
2020-07-02 19:06:57 +0500

    Send a sample HTTP request

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
 import System.Environment
+import Network.HTTP.Simple
 
 main = do  
     args <- getArgs
-    putStrLn (args!!0 ++ " " ++ args!!1)
+    putStrLn ("ServerUrl: " ++ args!!0 ++ "; PlayerKey: " ++ args!!1)
+    request <- parseRequest (args!!0 ++ "?playerKey=" ++ args!!1)
+    httpLBS request
