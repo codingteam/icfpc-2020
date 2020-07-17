29e4529e8be430fc7c10c9f45f8515954bfe3d4d
Alexander Batischev <eual.jp@gmail.com>
2020-07-17 18:13:04 +0300

    Implement decrement

--- tools/reducer/src/Reducer.hs
+++ tools/reducer/src/Reducer.hs
@@ -7,6 +7,7 @@ data Op =
     Number Int
   | Ap
   | Inc
+  | Dec
   deriving (Show, Eq)
 
 reduce :: [Op] -> [Op]
@@ -14,4 +15,5 @@ reduce program = go [] (reverse program)
   where
   go stack [] = stack
   go (Inc:(Number value):stack) (Ap:rest) = go (Number (value + 1):stack) rest
+  go (Dec:(Number value):stack) (Ap:rest) = go (Number (value - 1):stack) rest
   go stack (op:rest) = go (op:stack) rest
--- tools/reducer/test/Spec.hs
+++ tools/reducer/test/Spec.hs
@@ -20,4 +20,13 @@ specs = testGroup "Tests from specificaton"
     , testCase "#5.7" $ reduce [Ap, Inc, Number (-1)] @?= [Number 0]
     , testCase "#5.8" $ reduce [Ap, Inc, Number (-2)] @?= [Number (-1)]
     , testCase "#5.9" $ reduce [Ap, Inc, Number (-3)] @?= [Number (-2)]
+
+    , testCase "#6.1" $ reduce [Ap, Dec, Number 1] @?= [Number 0]
+    , testCase "#6.2" $ reduce [Ap, Dec, Number 2] @?= [Number 1]
+    , testCase "#6.3" $ reduce [Ap, Dec, Number 3] @?= [Number 2]
+    , testCase "#6.4" $ reduce [Ap, Dec, Number 4] @?= [Number 3]
+    , testCase "#6.5" $ reduce [Ap, Dec, Number 1024] @?= [Number 1023]
+    , testCase "#6.6" $ reduce [Ap, Dec, Number 0] @?= [Number (-1)]
+    , testCase "#6.7" $ reduce [Ap, Dec, Number (-1)] @?= [Number (-2)]
+    , testCase "#6.8" $ reduce [Ap, Dec, Number (-2)] @?= [Number (-3)]
   ]
