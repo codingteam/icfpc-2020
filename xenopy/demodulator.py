69d5a74754c8d60fc937140b509549b0eb975963
IngvarJackal <awoo :P>
2020-07-18 20:25:55 +0200

    move tests into module

--- xenopy/demodulator.py
+++ xenopy/demodulator.py
@@ -67,28 +67,3 @@ def demodulate_list(string):
     nested_list = eval("".join(result))[0]
 
     return nested_list
-
-assert demodulate_list("010") == 0 # 0
-assert demodulate_list("01100001") == 1 # 1
-assert demodulate_list("10100001") == -1 # -1
-assert demodulate_list("01100010") == 2 # 2
-assert demodulate_list("10100010") == -2 # -2
-
-assert demodulate_list("0111000010000") == 16 # 16
-assert demodulate_list("1011000010000") == -16 # -16
-
-assert demodulate_list("0111011111111") == 255 # 255
-assert demodulate_list("1011011111111") == -255 # -255
-
-assert demodulate_list("011110000100000000") == 256 # 256
-assert demodulate_list("101110000100000000") == -256 # -256
-
-assert demodulate_list("11 00 00".replace(" ", "")) == [None, None]
-assert demodulate_list("11 010 00".replace(" ", "")) == [0, None]
-
-assert demodulate_list("11 01100001 01100010".replace(" ", "")) == [1, 2]
-assert demodulate_list("11 01100001 11 01100010 00".replace(" ", "")) == [1, 2, None]
-
-assert demodulate_list("11 01100001 11 01100010 11 00 11 00 00".replace(" ", "")) == [1, 2, None, None, None]
-
-assert demodulate_list("11 01100001 11 11 01100010 11 01100011 00 11 01100100 00".replace(" ", "")) == [1, [2, 3, None], 4, None]
