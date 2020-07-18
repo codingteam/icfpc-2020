8a027924faa116a85adcc885eff3e6ad7c1b7b6e
Friedrich von Never <friedrich@fornever.me>
2020-07-18 18:15:57 +0700

    Evaluator improvements, tracing

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,6 +1,7 @@
 module Evaluator (evaluateSymbol) where
 
 import Data.IntMap ((!))
+import Debug.Trace
 
 import Reducer
 
@@ -8,14 +9,16 @@ getExpr :: DefId -> Program -> ExprTree
 getExpr id program = program ! id
 
 evaluateExpr :: ExprTree -> Program -> ExprTree
-evaluateExpr (Number x) _ = Number x
+evaluateExpr (DefValue v) p =
+  let newExpr = traceShowId $ getExpr v p in
+  trace "# (DefValue v)" $ evaluateExpr newExpr p
 evaluateExpr (Ap (DefValue v) x) p =
-  let newExpr = Ap (getExpr v p) x in
-  simplify newExpr
+  let newExpr = traceShowId $ Ap (getExpr v p) x in
+  trace "# (Ap (DefValue v) x)" $ evaluateExpr (simplify newExpr) p
 evaluateExpr (Ap f (DefValue v)) p =
-  let newExpr = Ap f (getExpr v p) in
-  simplify newExpr
-evaluateExpr e _ = error $ "Cannot evaluate expression " ++ (show e)
+  let newExpr = traceShowId $ Ap f (getExpr v p) in
+  trace "# (Ap f (DefValue v))" $ evaluateExpr (simplify newExpr) p
+evaluateExpr e _ = e
 
 evaluateSymbol :: DefId -> Program -> ExprTree
 evaluateSymbol id program =
