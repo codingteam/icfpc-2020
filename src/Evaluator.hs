fd15f998ab6909849f480ed21098c28622b5ffde
Alexander Batischev <eual.jp@gmail.com>
2020-07-18 22:58:47 +0300

    Use `Reducer.evaluate` instead of `Evaluator`

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -19,21 +19,7 @@ evaluateExpr' :: ExprTree -> Program -> ExprTree
 evaluateExpr' e p = fromMaybe e $ evaluateExpr e p
 
 evaluateExpr :: ExprTree -> Program -> Maybe ExprTree
-evaluateExpr orig@(DefValue v) p =
-  let newExpr = getExpr v p in
-  Just $ evaluateExprGuarded "(DefValue v)" orig newExpr p
-evaluateExpr orig@(Ap (DefValue v) x) p =
-  let newExpr = Ap (getExpr v p) x in
-  Just $ evaluateExprGuarded "(Ap (DefValue v) x)" orig newExpr p
-evaluateExpr orig@(Ap f (DefValue v)) p =
-  let newExpr = Ap (simplify f) (getExpr v p) in
-  Just $ evaluateExprGuarded "(Ap f (DefValue v))" orig newExpr p
-evaluateExpr orig@(Ap f x) p = do
-  f' <- evaluateExpr f p
-  x' <- evaluateExpr x p
-  let newExpr = Ap f' x' 
-  Just $ evaluateExprGuarded "(Ap f x)" orig newExpr p
-evaluateExpr e _ = Nothing
+evaluateExpr orig p = Just $ evaluate p orig
 
 evaluateSymbol :: DefId -> Program -> ExprTree
 evaluateSymbol id program =
