64284ce04121050c3e733b476f94add05fd2569b
Friedrich von Never <friedrich@fornever.me>
2020-07-18 15:51:02 +0700

    Fix compilation in master

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -7,16 +7,16 @@ import Types
 
 type UniqueId = Int
 
-data GlobalState = GlobalState
-  { gsProg    :: Map String ExprTree
-  , gsMemory  :: Map Ptr Value
-  , gsCounter :: UniqueId
-  }
-  deriving (Show)
+--data GlobalState = GlobalState
+--  { gsProg    :: Map String ExprTree
+--  , gsMemory  :: Map Ptr Value
+--  , gsCounter :: UniqueId
+--  }
+--  deriving (Show)
 
 data Error = ETooFewArgs
 
-type EvalM a = State GlobalState a
+--type EvalM a = State GlobalState a
 
 numArgs :: Operation -> Int
 numArgs Add = 2
@@ -38,14 +38,14 @@ numArgs Cons = 2
 numArgs Nil = 0
 numArgs IsNil = 1
 
-eval :: Expr -> Expr
-eval (Ap f []) = f
-eval (Ap (Ap f xs) ys) = eval $ Ap f (xs ++ ys)
-eval e@(Ap (Op op) xs) =
-  if length xs == numArgs op
-    then evalOp op xs
-    else if length xs > numArgs op
-           then error "too many args"
-           else e
-eval e = e
+--eval :: Expr -> Expr
+--eval (Ap f []) = f
+--eval (Ap (Ap f xs) ys) = eval $ Ap f (xs ++ ys)
+--eval e@(Ap (Op op) xs) =
+--  if length xs == numArgs op
+--    then evalOp op xs
+--    else if length xs > numArgs op
+--           then error "too many args"
+--           else e
+--eval e = e
 
