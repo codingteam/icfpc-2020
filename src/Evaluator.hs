ed1b0e16570519488d57909a5c5f6030ff0bb892
Friedrich von Never <friedrich@fornever.me>
2020-07-18 19:21:02 +0700

    New guarded evaluator

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -8,16 +8,26 @@ import Reducer
 getExpr :: DefId -> Program -> ExprTree
 getExpr id program = program ! id
 
+evaluateExprGuarded :: String -> ExprTree -> ExprTree -> Program -> ExprTree
+evaluateExprGuarded label original new program =
+  if original == new
+  then original
+  else trace ("# " ++ label ++ ": \norig: " ++ (show original) ++ "\nnew: " ++ (show new)) $
+       evaluateExpr (simplify new) program
+
 evaluateExpr :: ExprTree -> Program -> ExprTree
-evaluateExpr (DefValue v) p =
-  let newExpr = traceShowId $ getExpr v p in
-  trace "# (DefValue v)" $ evaluateExpr newExpr p
-evaluateExpr (Ap (DefValue v) x) p =
-  let newExpr = traceShowId $ Ap (getExpr v p) x in
-  trace "# (Ap (DefValue v) x)" $ evaluateExpr (simplify newExpr) p
-evaluateExpr (Ap f (DefValue v)) p =
-  let newExpr = traceShowId $ Ap f (getExpr v p) in
-  trace "# (Ap f (DefValue v))" $ evaluateExpr (simplify newExpr) p
+evaluateExpr orig@(DefValue v) p =
+  let newExpr = getExpr v p in
+  evaluateExprGuarded "(DefValue v)" orig newExpr p
+evaluateExpr orig@(Ap (DefValue v) x) p =
+  let newExpr = Ap (getExpr v p) x in
+  evaluateExprGuarded "(Ap (DefValue v) x)" orig newExpr p
+evaluateExpr orig@(Ap f (DefValue v)) p =
+  let newExpr = Ap f (getExpr v p) in
+  evaluateExprGuarded "(Ap f (DefValue v))" orig newExpr p
+evaluateExpr orig@(Ap f x) p =
+  let newExpr = Ap (evaluateExpr f p) (evaluateExpr x p) in
+  evaluateExprGuarded "(Ap f x)" orig newExpr p
 evaluateExpr e _ = e
 
 evaluateSymbol :: DefId -> Program -> ExprTree
