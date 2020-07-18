a8e5ab42a7fe60d5461693c3a6f81f129ffe4508
Alexander Batischev <eual.jp@gmail.com>
2020-07-18 23:13:11 +0300

    Simplify Evaluator's interface

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,4 +1,8 @@
-module Evaluator  where
+module Evaluator (
+    getExpr
+  , evaluateExpr
+  , evaluateSymbol
+) where
 
 import Data.IntMap ((!))
 import Debug.Trace
@@ -9,20 +13,10 @@ import Reducer
 getExpr :: DefId -> Program -> ExprTree
 getExpr id program = program ! id
 
-evaluateExprGuarded :: String -> ExprTree -> ExprTree -> Program -> ExprTree
-evaluateExprGuarded label original new program =
-  trace ("# " ++ label ++ ": \norig: " ++ (show original) ++ "\nnew: " ++ (show new)) $
-       let expr' = simplify new
-       in fromMaybe expr' $ evaluateExpr (simplify new) program
-
-evaluateExpr' :: ExprTree -> Program -> ExprTree
-evaluateExpr' e p = fromMaybe e $ evaluateExpr e p
-
-evaluateExpr :: ExprTree -> Program -> Maybe ExprTree
-evaluateExpr orig p = Just $ evaluate p orig
+evaluateExpr :: ExprTree -> Program -> ExprTree
+evaluateExpr e p = evaluate p e
 
 evaluateSymbol :: DefId -> Program -> ExprTree
 evaluateSymbol id program =
   let expr = getExpr id program in
-  trace (show expr) $
-    fromMaybe expr $ evaluateExpr expr program
+  trace (show expr) $ evaluateExpr expr program
