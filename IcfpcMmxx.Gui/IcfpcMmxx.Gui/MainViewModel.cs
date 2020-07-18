8a098685e6ae66f64a2a44150bafa4ddc2f45586
Friedrich von Never <friedrich@fornever.me>
2020-07-18 22:06:09 +0700

    Initial UI implementation for drawing

---
+++ IcfpcMmxx.Gui/IcfpcMmxx.Gui/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace IcfpcMmxx.Gui
+{
+    public class MainViewModel
+    {
+        private readonly Action _invalidate;
+
+        public MainViewModel(Action invalidate)
+        {
+            _invalidate = invalidate;
+        }
+
+        public WriteableBitmap Bitmap { get; } = new WriteableBitmap(
+            new PixelSize(640, 480),
+            new Vector(96.0, 96.0),
+            PixelFormat.Bgra8888);
+
+        public unsafe void SetPixel(double dx, double dy, Color color)
+        {
+            var x = (int) dx;
+            var y = (int) dy;
+
+            var pixel = color.B + (color.G << 8) + (color.R << 16) +
+                     (color.A << 24);
+
+            using var fb = Bitmap.Lock();
+            var ptr = (int*) fb.Address;
+            ptr += Bitmap.PixelSize.Width * y + x;
+
+            *ptr = pixel;
+
+            _invalidate();
+        }
+    }
+}
---
+++ IcfpcMmxx.Gui/IcfpcMmxx.Gui/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Threading;
+
+namespace IcfpcMmxx.Gui
+{
+    public class MainWindow : Window
+    {
+        private MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.AttachDevTools();
+            DataContext = _viewModel;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+
+            var grid = (Grid) Content;
+            var image = grid.Children.First();
+
+            _viewModel = new MainViewModel(
+                () => Dispatcher.UIThread.InvokeAsync(() => image.InvalidateVisual()));
+
+            image.PointerMoved += ImageOnPointerMoved;
+        }
+
+        private (double, double) TranslatePosition(PointerEventArgs ea, Image image)
+        {
+            var pos = ea.GetPosition(image);
+            return (pos.X, pos.Y);
+        }
+
+        private void ImageOnPointerMoved(object sender, PointerEventArgs e)
+        {
+            var (x, y) = TranslatePosition(e, (Image)sender);
+            _viewModel.SetPixel(x, y, Colors.White);
+        }
+    }
+}
