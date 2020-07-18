7a1ee464688d8167b082247b079986851e0e390c
Denis Mukhametianov <denis.mukhametianov@jetbrains.com>
2020-07-19 00:49:40 +0300

    csharp executor: split to files

--- csmodulator/Modulator/Executor.Tests/SingleFuncTests.cs
+++ csmodulator/Modulator/Executor.Tests/SingleFuncTests.cs
@@ -1,44 +1,8 @@
-using System;
-using System.IO;
+﻿using System;
 using NUnit.Framework;
 
 namespace Executor.Tests
 {
-    public class ProgramTests
-    {
-        [Test]
-        public void Test()
-        {
-            var add4 = "add4 = ap add ap add ap add ap add 4";
-            var main = "ap add4 8 5 5 5 5 5 3 3 3";
-            var fff = "fff = ap t fff";
-            var main1 = "ap fff 42";
-            var executor = new ProgramExecutor();
-            var result = executor.Execute(main, add4);
-            //var result = executor.Execute(main1, fff);N
-            Console.WriteLine(result.PrettyPrint());
-            Console.WriteLine(result.Print());
-            Console.WriteLine(AstReducer.ReducesCount);
-        }
-
-        [Test]
-        public void RunGalaxy()
-        {
-            var declarations = File.ReadAllLines("galaxy.txt");
-            var main = "ap galaxy ap ap cons 0 146";
-            var executor = new ProgramExecutor();
-            var result = executor.Execute(main, declarations);
-            Console.WriteLine(result.PrettyPrint());
-        }
-
-        [Test]
-        public void Test1()
-        {
-            var x = "ap ap s s ap ap ap s t x0 x1";
-            Console.WriteLine(AstReducer.Reduce(new AstParser(new FunctionDeclarationsFactory()).Parse(x)).PrettyPrint());
-        }
-    }
-
     public class SingleFuncTests
     {
         [SetUp]
