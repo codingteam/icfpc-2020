00bc8a3ba3d4575f732ae7296c07c0918d7009b2
💮 <pink_snow@t.me>
2020-07-19 09:29:05 +0000

    multishot interactor

--- app/interactor/Main.hs
+++ app/interactor/Main.hs
@@ -1,20 +1,38 @@
 module Main where
 
+import Control.Monad (when)
 import System.Environment (getArgs)
+import System.Exit (exitSuccess)
+import System.IO (isEOF)
 
 import qualified Invaluator as I
 
 main :: IO ()
 main = do
   args <- getArgs
-  let actualArgs = case args of
-                     [state, dx, dy] -> ["galaxy", "data/galaxy.txt", state, dx, dy]
-                     full@[symbol, filePath, state, dx, dy] -> full
-                     _ -> error "Usage: interactor [<symbol> <filePath>] <state> <dx> <dy>"
-  let [symbol, filePath, state, dx, dy] = actualArgs
+  case args of
+   [] -> multiShot "data/galaxy.txt"
+   [filePath] -> multiShot filePath
+   [state, dx, dy] -> oneShot "galaxy" "data/galaxy.txt" state dx dy
+   [symbol, filePath, state, dx, dy] -> oneShot symbol filePath state dx dy
+   _ -> error "Usage: interactor [<symbol> <filePath>] [<state> <dx> <dy>]"
+
+oneShot symbol filePath state dx dy = do
   symbolValue <- I.loadSymbol filePath symbol
   state <- I.loadSymbolContents ("galaxy = " ++ state) "galaxy"
   state' <- I.evalData state
   result <- I.interact symbolValue state' (read dx) (read dy)
   let result' = I.alienShow result
   putStrLn $ "+++" ++ result'
+
+multiShot filePath = do
+  galaxy <- I.loadGalaxy filePath
+  interactiveLoop galaxy I.DNil
+
+interactiveLoop galaxy state = do
+  eof <- isEOF
+  when eof exitSuccess
+  [x, y] <- (map read . words) <$> getLine
+  result@(I.InteractResult _ state' _) <- I.interact galaxy state x y
+  putStrLn $ "+++" ++ I.alienShow result
+  interactiveLoop galaxy state'
