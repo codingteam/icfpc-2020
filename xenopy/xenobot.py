7145552d587a2e6d41055897a9c4db4e096e0d89
IngvarJackal <awoo :P>
2020-07-19 19:46:24 +0200

    change gravity constant 2

--- xenopy/xenobot.py
+++ xenopy/xenobot.py
@@ -35,7 +35,7 @@ def send_request(data):
 
 init_data = send_request([2, player_key, []])
 is_running = True
-gravity_constant = 1.25
+gravity_constant = 0.75
 desired_orbit_in_moon_radiuses = 2.5
 
 try:
