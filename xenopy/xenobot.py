5dd667120451c46a9bbc2e02c98b96966b3ddffb
IngvarJackal <awoo :P>
2020-07-19 20:33:11 +0200

    what if gravity is constant?

--- xenopy/xenobot.py
+++ xenopy/xenobot.py
@@ -35,7 +35,7 @@ def send_request(data):
 
 init_data = send_request([2, player_key, []])
 is_running = True
-gravity_constant = 0.5
+gravity_constant = 2.5
 desired_orbit_in_moon_radiuses = 2
 
 try:
@@ -75,9 +75,7 @@ while is_running:
         for ship in parsed_data.our_fleet:
             # try to orbit
 
-            desired_orbital_velocity = math.sqrt(
-                parsed_data.moon_radius**2 * gravity_constant / desired_orbit_in_moon_radiuses
-            )
+            desired_orbital_velocity = 5
             current_velocity = get_vector_magnitude(ship.xy_velocity)
             print("desired_orbital_velocity", desired_orbital_velocity, " | current_velocity", current_velocity)
 
