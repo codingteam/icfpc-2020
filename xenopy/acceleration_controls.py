61803e1bc12a56660853a377533706cf0c9738f1
IngvarJackal <awoo :P>
2020-07-20 00:58:36 +0200

    modularize

---
+++ xenopy/acceleration_controls.py
@@ -0,0 +1,60 @@
+from state_parsing import *
+
+gravity_constant = 0.004
+distances = []
+
+def get_rotated_vector(vector):
+    return [-vector[1], vector[0]]
+
+def get_vector_magnitude(vector):
+    return math.sqrt(vector[0]**2 + vector[1]**2)
+
+def normalize_vector(vector):
+    magnitude = get_vector_magnitude(vector)
+    if magnitude == 0:
+        return [0 ,0]
+    return [
+        round(vector[0]/magnitude),
+        round(vector[1]/magnitude)
+    ]
+
+def calculate_acceleration(ship: Ship, moon_radius: int, desired_orbit_over_moon = 30):
+    desired_orbit_height = moon_radius * math.sqrt(2) + desired_orbit_over_moon
+    desired_orbital_velocity = math.sqrt(
+        moon_radius ** 2 * gravity_constant / desired_orbit_height
+    ) * desired_orbit_height  # convert to linear speed
+    current_velocity = get_vector_magnitude(ship.xy_velocity)
+
+    current_distance = get_vector_magnitude(ship.xy_coordinates)
+    distances.append(current_distance)
+    print(
+        "desired orbital velocity {:.1f}, current velocity {:.1f}".format(
+            desired_orbital_velocity, current_velocity
+        ),
+        " | desired distance {:.1f}, current distance {:.1f}, avg distance {:.1f} ".format(
+            desired_orbit_height, current_distance, sum(distances[:10]) / len(distances[:10])
+        )
+    )
+
+    if current_distance < desired_orbit_over_moon:
+        velocity_error_boundary = 1 * current_distance / desired_orbit_over_moon
+    else:
+        velocity_error_boundary = max(1, (15 - abs(current_distance - desired_orbit_over_moon))/10)
+
+    print("velocity boundary {:.1f}".format(velocity_error_boundary))
+    if current_velocity - desired_orbital_velocity < -velocity_error_boundary:  # too slow
+        print("fixing too slow speed {:.1f}".format(
+            current_velocity - desired_orbital_velocity))
+        new_vector = get_rotated_vector(ship.xy_coordinates)  # rotate
+    elif current_velocity - desired_orbital_velocity > velocity_error_boundary:  # too fast
+        print("fixing too fast speed {:.1f}".format(
+            current_velocity - desired_orbital_velocity))
+        new_vector = ship.xy_velocity  # slow down
+    else:
+        print("speed in boundaries")
+        new_vector = [0, 0]
+
+    acceleration_vector = normalize_vector(new_vector)
+    print("new_vector:", new_vector, "acceleration_vector", acceleration_vector)
+
+    return acceleration_vector
--- xenopy/xenobot.py
+++ xenopy/xenobot.py
@@ -6,6 +6,7 @@ import requests
 from demodulator import demodulate_list
 from modulator import modulate
 from state_parsing import *
+from acceleration_controls import *
 
 url = sys.argv[1]
 player_key = int(sys.argv[2])
@@ -34,8 +35,6 @@ def send_request(data):
 
 init_data = send_request([2, player_key, []])
 is_running = True
-gravity_constant = 0.004
-desired_orbit_over_moon = 25
 
 try:
     print("-" * 30)
@@ -52,71 +51,13 @@ except Exception:
     print(traceback.format_exc())
 
 
-def get_rotated_vector(vector):
-    return [-vector[1], vector[0]]
-
-def get_vector_magnitude(vector):
-    return math.sqrt(vector[0]**2 + vector[1]**2)
-
-def normalize_vector(vector):
-    magnitude = get_vector_magnitude(vector)
-    if magnitude == 0:
-        return [0 ,0]
-    return [
-        round(vector[0]/magnitude),
-        round(vector[1]/magnitude)
-    ]
-
-def calculate_acceleration(parsed_data):
-    desired_orbit_height = parsed_data.moon_radius * math.sqrt(2) + desired_orbit_over_moon
-    desired_orbital_velocity = math.sqrt(
-        parsed_data.moon_radius ** 2 * gravity_constant / desired_orbit_height
-    ) * desired_orbit_height  # convert to linear speed
-    current_velocity = get_vector_magnitude(ship.xy_velocity)
-
-    current_distance = get_vector_magnitude(ship.xy_coordinates)
-    distances.append(current_distance)
-    print(
-        "desired orbital velocity {:.1f}, current velocity {:.1f}".format(
-            desired_orbital_velocity, current_velocity
-        ),
-        " | desired distance {:.1f}, current distance {:.1f}, avg distance {:.1f} ".format(
-            desired_orbit_height, current_distance, sum(distances[:10]) / len(distances[:10])
-        )
-    )
-
-    if current_distance < desired_orbit_over_moon:
-        velocity_error_boundary = 1 * current_distance / desired_orbit_over_moon
-    else:
-        velocity_error_boundary = max(1, (15 - abs(current_distance - desired_orbit_over_moon))/10)
-
-    print("velocity boundary {:.1f}".format(velocity_error_boundary))
-    if current_velocity - desired_orbital_velocity < -velocity_error_boundary:  # too slow
-        print("fixing too slow speed {:.1f}".format(
-            current_velocity - desired_orbital_velocity))
-        new_vector = get_rotated_vector(ship.xy_coordinates)  # rotate
-    elif current_velocity - desired_orbital_velocity > velocity_error_boundary:  # too fast
-        print("fixing too fast speed {:.1f}".format(
-            current_velocity - desired_orbital_velocity))
-        new_vector = ship.xy_velocity  # slow down
-    else:
-        print("speed in boundaries")
-        new_vector = [0, 0]
-
-    acceleration_vector = normalize_vector(new_vector)
-    print("new_vector:", new_vector, "acceleration_vector", acceleration_vector)
-
-    return acceleration_vector
-
-distances = []
 while is_running:
     try:
         print("-" * 30)
         commands = []
         for ship in parsed_data.our_fleet:
             # try to orbit
-
-            acceleration_vector = calculate_acceleration(parsed_data)
+            acceleration_vector = calculate_acceleration(ship, parsed_data.moon_radius)
             if acceleration_vector != [0, 0]:
                 commands.append([
                     0,  # acceleration command
