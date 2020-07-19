c85b1c32f791784b754b9654cd53a9ccfbef25c2
Alexander Batischev <eual.jp@gmail.com>
2020-07-20 00:51:35 +0300

    Modulate pairs and lists differently
    A pair is always `cons x y`, i.e. `11 x y`. A list is either `nil`
    (`00`) or `cons x …` (11 x 00, 11 x 11 y 00 etc.)

--- xenopy/modulator.py
+++ xenopy/modulator.py
@@ -4,6 +4,8 @@ def modulate(x):
         return mod_list(x)
     elif isinstance(x, int):
         return mod_number(x)
+    elif isinstance(x, tuple):
+        return mod_tuple(x)
     else:
         raise Exception(f"Unsupported data: {x}")
 
@@ -25,13 +27,13 @@ def mod_number(x):
     return signum + len_prefix + binary
 
 def mod_list(lst):
-    if not lst:
+    if lst == []:
         return '00'
-    if len(lst) == 2:
-        return '11' + modulate(lst[0]) + modulate(lst[1])
     result = ''
     for x in lst:
         result += '11' + modulate(x)
     result += '00'
     return result
 
+def mod_tuple(x):
+    return '11' + modulate(x[0]) + modulate(x[1])
