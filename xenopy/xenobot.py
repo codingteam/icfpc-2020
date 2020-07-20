e13af8a51bd9756a1835e4687e386639c8a43aa4
Alexander Batischev <eual.jp@gmail.com>
2020-07-20 09:52:18 +0300

    Give spawned ships some fuel and ammo

--- xenopy/xenobot.py
+++ xenopy/xenobot.py
@@ -74,7 +74,10 @@ def play_a_turn():
         # Wait for the "mother ship" to reach stable orbit, then start spawning
         if acceleration_command is None and ship.x4[3] > 1:
             print("Ship {} spawns another ship!".format(ship.ship_id))
-            commands.append([3, ship.ship_id, [0, 0, 0, 1]])
+            fuel = int(ship.x4[3][0] / 2)
+            ammo = int(ship.x4[3][1] / 2)
+            subships = int(ship.x4[3][3] / 2)
+            commands.append([3, ship.ship_id, [fuel, ammo, 0, subships]])
 
     commands.extend(
         suggest_shooting_commands(
