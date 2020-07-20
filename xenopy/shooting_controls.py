c9d419288b26b95700f79bba51e9736c37e35bd5
IngvarJackal <awoo :P>
2020-07-20 14:39:05 +0200

    steal closest proximity shooting

--- xenopy/shooting_controls.py
+++ xenopy/shooting_controls.py
@@ -7,18 +7,50 @@ def next_position(current_position, velocity):
             current_position[1] + velocity[1]
             )
 
+def distance(x, y):
+    dx = x[0] - y[0]
+    dy = x[1] - y[1]
+    return max(abs(dx), abs(dy))
+
+def find_nearest_enemy(us_pos: (int, int), enemies: List[Ship]):
+    (distance, enemy) = (100500, None)
+
+    for candidate in enemies:
+        d = distance(us_pos, candidate.xy_coordinates)
+        if d < distance:
+            distance = d
+            enemy = candidate
+
+    if distance < 15:
+        return enemy
+    else:
+        return None
+
+
 def suggest_shooting_commands(us: List[Ship], enemies: List[Ship]):
     print("[SHOOTING MODULE]")
     commands = []
 
-    ready_to_shoot = filter(lambda ship: ship.ship_params[1] != 0 and ship.x5 < (64-ship.ship_params[1]), us)
-    for (us, them) in zip(ready_to_shoot, enemies):
+    for s in us:
+        print("Ship {} has energy {}".format(s.ship_id, s.ship_params[1]))
+
+    ready_to_shoot = filter(lambda ship: ship.x4[1] != 0, us)
+    for us in ready_to_shoot:
+        them = find_nearest_enemy(us.xy_coordinates, enemies)
+        if not them:
+            continue
+
         target = next_position(them.xy_coordinates, them.xy_velocity)
+
+        if us.x5 > 62:
+            print(us, "is too hot to shoot")
+            continue
+
         commands.append([
             2, # shoot
             us.ship_id,
             target,
-            us.ship_params[1]
+            us.ship_params[1] / us.ship_params[3]
             ])
         print("Ship {} shooting at enemy {} at {} with power {}"
                 .format(
