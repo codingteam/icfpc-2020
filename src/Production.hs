10e7dabbc03f5c4735c71c55ba3e65ba90d2d8f8
Friedrich von Never <friedrich@fornever.me>
2020-07-20 17:24:38 +0700

    Fix ship types for the Detonation experiment

--- src/Production.hs
+++ src/Production.hs
@@ -24,10 +24,10 @@ startMessage playerKey fuel guns type_ x = Start playerKey fuel guns (shipTypeTo
 findMyShip :: ServerResponse -> ShipType -> Maybe ShipId
 findMyShip (Success _ (Just sgi) (Just (GameState _ _ shipData))) type_ =
   let myRole = sgiRole sgi
-      ships = map fst shipData 
+      ships = map fst shipData
   in listToMaybe $ map shipId $ filter (isShipOfType type_) ships
   where isShipOfType type_ ship = (shipType $ shipParameters ship) == type_
-  
+
 findMyShip _ _ = Nothing
 
 --
@@ -41,35 +41,35 @@ production playerKey talkWithAliens = do
   errPutStrLn "----- Sending Join request -----"
   result <- talkWithAliens $ joinMessage playerKey
   errPutStrLn $ "Join result: " ++ show result
-  let response = decodeResponse result 
-  errPutStrLn $ "Join result (decoded): " ++ show response 
+  let response = decodeResponse result
+  errPutStrLn $ "Join result (decoded): " ++ show response
   errPutStrLn "----- Sending Start request -----"
   result <- talkWithAliens $ startMessage playerKey 5 10 Mothership 20
   errPutStrLn $ "Start result: " ++ show result
-  let response = decodeResponse result 
+  let response = decodeResponse result
   errPutStrLn $ "Start result (decoded): " ++ show response
-  
-  response <- spawnShip response
-  response <- if isAttacker response 
-    then do destroyMyShip response Default
-    else do destroyMyShip response Mothership 
-  
+
+  response <- spawnShip response Zero
+  response <- if isAttacker response
+    then do destroyMyShip response Zero
+    else do destroyMyShip response Mothership
+
   forever $ do
     doNothing
 
   where
-  
+
   isAttacker (Success _ (Just info) _) =
     case sgiRole info of
       Attacker -> True
       Defender -> False
-  
-  spawnShip response = do
+
+  spawnShip response shipType = do
     errPutStrLn "----- Sending Spawn request -----"
     let (Just myShipId) = findMyShip response Mothership
-    result <- talkWithAliens $ Commands playerKey [Spawn myShipId $ ShipParameters 0 0 Zero 1]
+    result <- talkWithAliens $ Commands playerKey [Spawn myShipId $ ShipParameters 0 0 shipType 1]
     errPutStrLn $ "Spawn result: " ++ show result
-    let response = decodeResponse result 
+    let response = decodeResponse result
     errPutStrLn $ "Spawn result (decoded): " ++ show response
     return response
 
@@ -79,7 +79,7 @@ production playerKey talkWithAliens = do
     let (Just myShipId) = findMyShip response type_
     result <- talkWithAliens $ Commands playerKey [Detonate myShipId]
     errPutStrLn $ "Destroy result: " ++ show result
-    let response = decodeResponse result 
+    let response = decodeResponse result
     errPutStrLn $ "Destroy result (decoded): " ++ show response
     return response
 
@@ -87,5 +87,5 @@ production playerKey talkWithAliens = do
     errPutStrLn "----- Sending empty command list -----"
     result <- talkWithAliens $ Commands playerKey []
     errPutStrLn $ "Empty command list result: " ++ show result
-    let response = decodeResponse result 
+    let response = decodeResponse result
     errPutStrLn $ "Empty command list result (decoded): " ++ show response
