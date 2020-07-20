15f179ab5ae0c3e8e2156a9c3840edc59b264892
Friedrich von Never <friedrich@fornever.me>
2020-07-20 12:08:48 +0700

    Send Join request

--- src/Production.hs
+++ src/Production.hs
@@ -4,11 +4,19 @@ module Production
      ( production
      ) where
 
-import Invaluator (Data)
+import Control.Monad (forever)
+
+import Invaluator (Data(..))
+import HttpApi (sendMessageToAliens)
 import Modulator
 import Newtypes
+import Modulatable.Types (Command(..), CallToAliens(..))
 
+joinMessage :: PlayerKey -> CallToAliens
+joinMessage playerKey = CallToAliens Join playerKey Nothing Nothing
 
--- | TODO implement whatever logic is needed for production
 production :: PlayerKey -> (forall a. Modulatable a => a -> IO Data) -> IO ()
-production playerKey talkWithAliens = fail "TODO implement"
+production playerKey talkWithAliens = do
+  forever $ do
+    result <- talkWithAliens $ joinMessage playerKey
+    putStrLn $ show result
